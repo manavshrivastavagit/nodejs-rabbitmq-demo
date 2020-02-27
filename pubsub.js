f342faab083bcce94b6dd18ee38c31efe4af9429
manavshrivastava <manavshrivastava@hotmail.com>
2020-02-27 23:35:12 +0530

    rabbitmq demo

---
+++ pubsub.js
@@ -0,0 +1,82 @@
+const { execSync } = require("child_process");
+const amqp = require("amqplib");
+
+async function sleep(delay) {
+    return new Promise((resolve, reject) => {
+        setTimeout(resolve, delay);
+    });
+}
+
+async function createChannel(config) {
+    const { url, publishers, listeners } = Object.assign({url: "", publishers: {}, listeners: {}}, config);
+    try {
+        // create connection
+        const connection = await amqp.connect(url);
+        let channel = null;
+        connection._channels = [];
+        connection.on("error", (error) => {
+            console.error("Connection error : ", config, error);
+        });
+        connection.on("close", async (error) => {
+            if (channel) {
+                channel.close();
+            }
+            console.error("Connection close : ", config, error);
+            await sleep(1000);
+            createChannel(config);
+        });
+        // create channel
+        channel = await connection.createConfirmChannel();
+        channel.on("error", (error) => {
+            console.error("Channel error : ", config, error);
+        });
+        channel.on("close", (error) => {
+            console.error("Channel close : ", config, error);
+        });
+        // register listeners
+        for (queue in listeners) {
+            const callback = listeners[queue];
+            channel.assertQueue(queue, { durable: false });
+            channel.consume(queue, callback);
+        }
+        // publish
+        for (queue in publishers) {
+            const message = publishers[queue];
+            channel.assertQueue(queue, { durable: false });
+            channel.sendToQueue(queue, message);
+        }
+        return channel;
+    } catch (error) {
+        console.error("Create connection error : ", error);
+        await sleep(1000);
+        createChannel(config);
+    }
+}
+
+async function main() {
+    // publish "hello" message to queue
+    const channelPublish = await createChannel({
+        url: "amqp://0.0.0.0:5672",
+        publishers: {
+            "queue": Buffer.from("hello"),
+        }
+    });
+
+    // restart rabbitmq
+    execSync("docker stop rabbitmq");
+    execSync("docker start rabbitmq");
+
+    // consume message from queue
+    const channelConsume = await createChannel({
+        url: "amqp://0.0.0.0:5672",
+        listeners: {
+            "queue": (message) => {
+                console.log("Receive message ", message.content.toString());
+            },
+        }
+    });
+
+    return true;
+}
+
+main().catch((error) => console.error(error));
